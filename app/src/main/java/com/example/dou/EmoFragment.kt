7a871b0741e193012645d0b4627d93f92e6697f1
gahyunkim <mapledt001@naver.com>
2024-05-01 23:02:30 +0900

    Feat: Bucket 내에 저장되는 파일의 실행 문제, 저장 문제 해결 중(#13)
    - 버킷에 파일이 저장되는 것은 확인되지만, 해당 파일을 열어보았을 때 녹음파일이 제대로 실행되지 않는 문제점 발견
    => 해당 문제 해결하고 있음
    
    Related to: #13

--- app/src/main/java/com/example/dou/EmoFragment.kt
+++ app/src/main/java/com/example/dou/EmoFragment.kt
@@ -1,18 +1,20 @@
 package com.example.dou
 
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.dou.databinding.FragmentEmoBinding
 import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.speech.v1.LongRunningRecognizeRequest
+import com.google.cloud.speech.v1.LongRunningRecognizeResponse
 import com.google.cloud.speech.v1.RecognitionAudio
 import com.google.cloud.speech.v1.RecognitionConfig
-import com.google.cloud.speech.v1.RecognizeRequest
 import com.google.cloud.speech.v1.SpeechClient
 import com.google.cloud.speech.v1.SpeechSettings
 import com.google.cloud.storage.BlobInfo
@@ -21,6 +23,8 @@ import com.google.protobuf.ByteString
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
 
 
 class EmoFragment : Fragment() {
@@ -46,6 +50,10 @@ class EmoFragment : Fragment() {
         val fileUri = arguments?.getString("fileUri")
         if (fileUri != null) {
             Log.d("FileUri", "fileUri가 존재합니다: $fileUri")
+
+            // 여기 코드가 제대로 작동하는걸 보면 파일자체에 문제는 없어보임. file이 현재 버킷으로 저장되면서 발생하는 오류인 것 같음..
+//            val audioTest = readAudioFile(Uri.parse(fileUri))
+//            playAudioFromByteString(audioTest)
             uploadAudioToBucket(Uri.parse(fileUri))
         } else {
             Log.d("FileUri", "fileUri가 존재하지 않습니다.")
@@ -53,10 +61,11 @@ class EmoFragment : Fragment() {
     }
 
     private fun uploadAudioToBucket(audioUri: Uri) {
-        GlobalScope.launch(Dispatchers.IO) {
+        lifecycleScope.launch(Dispatchers.IO) {
             try {
                 // 오디오 파일을 읽고 Byte 배열로 변환
-                val audioData = readAudioFile(audioUri).toByteArray()
+                val audioData = readAudioFile(audioUri)
+                Log.d("오디오 데이터", audioData.joinToString(", "))
 
                 // Google Cloud Storage 클라이언트 초기화
                 val credentials = GoogleCredentials.fromStream(
@@ -64,13 +73,19 @@ class EmoFragment : Fragment() {
                 )
                 val storage = StorageOptions.newBuilder().setCredentials(credentials).build().service
 
+                val blobInfo = BlobInfo.newBuilder(bucketName, "audio_file.mp3")
+                    .setContentType("audio/mpeg")
+                    .build()
+
                 // 버킷에 오디오 파일 업로드
-                storage.create(BlobInfo.newBuilder(bucketName, "audio_file.wav").build(), audioData)
+                storage.create(blobInfo, audioData)
 
                 // 버킷에 업로드된 오디오 파일의 URI 생성
-                val audioUriForSpeechToText = "gs://$bucketName/audio_file.wav"
+                val audioUriForSpeechToText = "gs://$bucketName/audio_file.mp3"
 
                 Log.d("AudioURI", "오디오 파일이 버킷에 업로드되었습니다. URI: $audioUriForSpeechToText")
+
+                //playAudioFromCloudStorage(audioUriForSpeechToText)
 
                 // Speech-to-Text 클라이언트 초기화 및 변환 요청
                 initializeSpeechClient(audioUriForSpeechToText)
@@ -111,8 +126,8 @@ class EmoFragment : Fragment() {
                 Log.d("RecognitionAudio", "RecognitionAudio 설정 완료")
                 Log.d("오디오 요청", audio.toString())
 
-                // RecognitionRequest 설정
-                val request = RecognizeRequest.newBuilder()
+                // LongRunningRecognizeRequest 설정
+                val request = LongRunningRecognizeRequest.newBuilder()
                     .setConfig(config)
                     .setAudio(audio)
                     .build()
@@ -120,47 +135,101 @@ class EmoFragment : Fragment() {
                 Log.d("RecognitionRequest", "RecognitionRequest 설정 완료")
                 Log.d("요청 정보", request.toString())
 
-                // Speech-to-Text 변환 요청 보내기
-                val response = speechClient.recognize(request)
+//                val response =
+//                    speechClient.longRunningRecognizeAsync(config, audio).get()
 
-                Log.d("응답 정보", response.toString())
-                Log.d("RecognitionRequest", "Speech-to-Text 변환 요청 보내기 완료")
+                val response = speechClient.longRunningRecognizeAsync(request).get()
 
-                // 변환된 텍스트 처리
-                val resultText = response.resultsList.joinToString("\n") { result ->
-                    result.alternativesList.joinToString("\n") { alternative ->
-                        alternative.transcript
-                    }
-                }
-                Log.d("변환한 내용", resultText)
+//                while (!response.redone) {
+//                    Log.d("Response", "변환 중...")
+//                    withContext(Dispatchers.Main) {
+//                        binding.emoTxt.text = "변환 중..." // 작업이 완료될 때까지 UI 업데이트
+//                    }
+//                    delay(10000) // 10초마다 다시 확인
+//                }
+                Log.d("Response", "아직 변환 중...")
 
-                // 변환된 텍스트 표시
-                val finalText = "변환 완료\n$resultText"
-                Log.e("변환 과정", finalText)
+                Log.d("while문 탈출", "response.done이 드디어 true?")
+                handleResponse(response)
 
-                // UI 업데이트
-                launch(Dispatchers.Main) {
-                    // 변환 완료 메시지 토스트 표시
-                    Toast.makeText(requireContext(), "변환 완료", Toast.LENGTH_SHORT).show()
+//                val operation = async { speechClient.longRunningRecognizeCallable().call(request) }
+//
+//                // 결과 대기
+//                var response = operation.await()
+//
+//                while (!response.done) {
+//                    Log.d("Response", "변환 중...")
+//                    withContext(Dispatchers.Main) {
+//                        binding.emoTxt.text = "변환 중..." // 작업이 완료될 때까지 UI 업데이트
+//                    }
+//                    response = speechClient.getOperation(request.name)
+//                    delay(10000) // 10초마다 다시 확인
+//                }
+//                Log.d("while문 탈출", "response.done이 드디어 true?")
+//                handleResponse(response)
 
-                    // 변환된 텍스트 표시
-                    binding.emoTxt.text = finalText
-                }
-
-                // 클라이언트 종료
-                speechClient.close()
-
-                Log.d("SpeechClient", "Speech-to-Text 클라이언트 종료")
             } catch (e: Exception) {
                 Log.e("RecognitionRequest", "Speech-to-Text 변환 요청 중 오류 발생", e)
+            } finally {
+                // 클라이언트 종료
+                speechClient.close()
+                Log.d("SpeechClient", "Speech-to-Text 클라이언트 종료")
             }
         }
     }
 
-    private fun readAudioFile(audioUri: Uri): ByteString {
+    private fun playAudioFromByteString(audioData: ByteString) {
+        try {
+            // 바이트 스트림 데이터를 오디오 파일로 저장
+            val audioFile = File(requireContext().cacheDir, "audio_data.wav")
+            FileOutputStream(audioFile).use { outputStream ->
+                audioData.writeTo(outputStream)
+            }
+
+            // 저장된 오디오 파일을 재생
+            val mediaPlayer = MediaPlayer().apply {
+                setDataSource(audioFile.path)
+                prepare()
+                start()
+            }
+
+        } catch (e: Exception) {
+            Log.e("PlayAudio", "오디오 재생 중 오류 발생", e)
+        }
+    }
+
+    private fun handleResponse(response: LongRunningRecognizeResponse) {
+        // 결과 리스트를 가져옵니다.
+        val resultsList = response.resultsList
+
+        // 결과가 비어 있는지 확인합니다.
+        if (resultsList.isEmpty()) {
+            Log.d("handleResponse", "No results found")
+            return
+        }
+
+        // 결과가 있을 경우 각 결과를 처리합니다.
+        for (result in resultsList) {
+            // 결과의 alternative들을 가져와서 출력합니다.
+            val alternativesList = result.alternativesList
+            for (alternative in alternativesList) {
+                // 텍스트 추출
+                val transcript = alternative.transcript
+                // UI에 텍스트 표시 또는 다른 처리 수행
+                Log.d("handleResponse", "Transcript: $transcript")
+            }
+        }
+    }
+
+    /*private fun readAudioFile(audioUri: Uri): ByteString {
         requireContext().contentResolver.openInputStream(audioUri)?.use { inputStream ->
             return ByteString.readFrom(inputStream)
         } ?: throw IllegalStateException("InputStream이 null입니다.")
-    }
+    }*/
 
+    private fun readAudioFile(audioUri: Uri): ByteArray {
+        requireContext().contentResolver.openInputStream(audioUri)?.use { inputStream ->
+            return inputStream.readBytes()
+        } ?: throw IllegalStateException("InputStream이 null입니다.")
+    }
 }
